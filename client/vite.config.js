2149d62e487fc26cbee14da9248196a70fb954d0
unknown <mohammad.daaboul.99@gmail.com>
2024-08-07 19:57:01 +0300

    update vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Remove or comment out the proxy configuration for production
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:5100/api',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ihsan-omega.vercel.app/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
