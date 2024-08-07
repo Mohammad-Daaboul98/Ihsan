c1d0fb489d5af982026856b6bd63738dfad76ded
Mohammad <40797621+Mohammad-Daaboul98@users.noreply.github.com>
2024-08-07 13:40:56 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Ensure the output directory is 'dist'
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5100/api',
+        target: 'http://localhost:5100',
         changeOrigin: true,
-        rewrite: (path) => {
-          const newPath = path.replace(/^\/api/, '');
-          console.log(`Rewriting path: ${path} to ${newPath}`);
-          return newPath;
-        },
-      },
-    },
-  },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
