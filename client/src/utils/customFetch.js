6b5eb3b6d4dc52524db6f3b767d5253b1b55e284
Mohammad-Daaboul98 <mohammad.daaboul.99@gmail.com>
2024-11-01 22:51:13 +0300

    deploy

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: `${import.meta.env.URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_URL}/api/v1`,
 });
 
 export default customFetch;
--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  define: {
+     'process.env': {},
+  },
+  server: {
+    host: true,
+ },
   // server: {
   //   proxy: {
   //     "/api": {
