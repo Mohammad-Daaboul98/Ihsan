49d3fa96920be970bcf43d12116c57f676008137
Mohammad-Daaboul98 <mohammad.daaboul.99@gmail.com>
2024-12-23 18:30:58 +0300

    add new option to attend

--- server/shared/constants.js
+++ server/shared/constants.js
@@ -5,7 +5,7 @@ export const USER_TYPE = {
   admin: "admin",
 };
 
-export const STUDENT_RATE = {"ممتاز", "جيد جدا","جيد", "وسط", "سيء"};
+export const STUDENT_RATE = ["ممتاز", "جيد جدا","جيد", "وسط", "سيء"];
 
 export const STUDENT_ATTENDANCE = ["موجود", "غائب",'غياب مبرر'];
 
